fa4309454af5eeb2f5575b81d1157ce950449b2c
Kanishk-30 <kanishk30khadria@gmail.com>
2025-08-13 13:34:10 +0530

    Updated container padding for large screens

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
           DEFAULT: '1rem',
           sm: '1.5rem',
           lg: '2rem',
-          xl: '2.5rem',
-          '2xl': '3rem',
+          xl: '5rem',
+          '2xl': '8rem',
         },
         screens: {
           sm: '640px',
